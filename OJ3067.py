29a8171b764b50a6f74a3df0a9aeb31457a31851
Mitopptx <69070032@kmitl.ac.th>
2026-07-24 12:38:10 +0700

    Create OJ3067.py

---
+++ OJ3067.py
@@ -0,0 +1,9 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+if num1 > num2 and num2 > num3:
+  print("decreasing")
+elif num1 < num2 and num2 < num3:
+  print("increasing")
+else:
+  print("neither")
