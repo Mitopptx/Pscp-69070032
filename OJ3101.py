e493f349bfb82e21c8e5c264e4a05b32db032b36
Mitopptx <69070032@kmitl.ac.th>
2026-08-01 12:21:44 +0700

    Update OJ3101.py

--- OJ3101.py
+++ OJ3101.py
@@ -1,4 +1,10 @@
 number = int(input())
 degree = input()
 if degree == 'F':
-    
+    number = (number-32)*5/9
+if number <=0:
+    print("solid")
+elif number >= 100:
+    print("gas")
+else:
+    print("liquid")
