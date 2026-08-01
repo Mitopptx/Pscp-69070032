384484bbdf6852a15c9108029de42285df41c1b7
Mitopptx <69070032@kmitl.ac.th>
2026-08-01 14:32:29 +0700

    Create OJ3107.py

---
+++ OJ3107.py
@@ -0,0 +1,17 @@
+status,year,salary=input().split()
+year = int(year)
+salary = int(salary)
+if year <5:
+    per =4
+elif year >10:
+    per=6
+else:
+    per=5
+if status =="M":
+    per += per//1.5
+    print(1500+int(per*salary/100))
+elif status == "B":
+    per +=1
+    print(1000+int(per*salary/100))
+elif status == "G":
+    print(500+int(per*salary/100))
