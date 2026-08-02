e47358093cf5ac553a90f6c593ebf171ba220581
Mitopptx <69070032@kmitl.ac.th>
2026-08-02 13:05:13 +0700

    Create OJ3115.py

---
+++ OJ3115.py
@@ -0,0 +1,13 @@
+store,check = map(int,input().split())
+arrs = [0]*store
+for i in range(store):
+    arrs[i] = input()
+strcheck = input()
+timecheck = strcheck.split(" ")
+for i in range(check):
+    count =0
+    for j in range(store):
+        start,stop= arrs[j].split()
+        if int(start)<= int(timecheck[i]) < int(stop):
+            count +=1
+    print(count,end=" ")
