fcf4083c3cb979ae089e0dc80536500d3d6cbdb2
Mitopptx <69070032@kmitl.ac.th>
2026-08-15 20:20:48 +0700

    Update OJ3230.py

--- OJ3230.py
+++ OJ3230.py
@@ -16,7 +16,26 @@ else:
 if int(number[::1]) == int(number[::-1]):
     if int(number[0])+int(number[4])>5:
         answer += "1"
-    elif int(number[2])-int(number[4])>5:
+    elif int(number[1])*int(number[3])>5:
         answer +="2"
     else:
         answer +="0"
+else:
+     if int(number[0]) // int(number[4])>5:
+         answer +="1"
+     elif int(number[1]) - int(number[4])>5:
+         answer +="2"
+     else:
+         answer += "0"
+total1 = 0
+total2 = 1
+for i in range(5):
+    total1 += int(number[i])
+    total2 *= int(number[i])
+if total1 >25:
+    answer+= "1"
+elif total2> 55:
+    answer += "2"
+else:
+    answer += "0"
+print(answer)
