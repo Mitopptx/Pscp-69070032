85d07b7706444f12bf1e6117a275c90d8c0106e7
Mitopptx <69070032@kmitl.ac.th>
2026-08-16 22:35:14 +0700

    w

--- OJ3226.py
+++ OJ3226.py
@@ -1,6 +1,7 @@
 """ZIP ZIP ZIP"""
 money = float(input())
 year = int(input())
-for year in range(year):
-    money += (money *0.0381*1000//10)/100
-print(money)
+money = int(money * 100)
+for _ in range(year):
+    money += money * 381 // 10000
+print(f"{money // 100}.{money % 100:02d}")
--- w.py
+++ w.py
@@ -1,1 +1,13 @@
-print("-1")
+"""w"""
+a = int(input())
+b = int(input())
+goal = int(input())
+buse = goal//5
+if buse <=b:
+    goal -= buse*5
+else:
+    goal -= b*5
+if goal<=a:
+    print(goal)
+else:
+    print("-1")
