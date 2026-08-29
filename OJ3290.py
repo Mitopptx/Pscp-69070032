aca9ff29e714ef0981cfab585b9fd7c4858859fc
Mitopptx <69070032@kmitl.ac.th>
2026-08-29 13:53:34 +0700

    Update OJ3290.py

--- OJ3290.py
+++ OJ3290.py
@@ -3,10 +3,10 @@ def main():
     """Arrow"""
     k = int(input())
     n = int(input())
-    for i in range(1,n+1):
-            print(" "*(n-i),end="")
-            print("*"*k)
-    for i in range(1,n):
-            print(" "*(i),end="")
-            print("*"*k)
+    for i in range(1,int(n/2)+1):
+        print(" "*(int(n/2)-i+1),end="")
+        print("*"*k)
+    for i in range(int(n/2)+1):
+        print(" "*(i),end="")
+        print("*"*k)
 main()
