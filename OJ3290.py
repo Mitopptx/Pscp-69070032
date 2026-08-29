538645340ec6f952b91aac5df28199b344a432b2
Mitopptx <69070032@kmitl.ac.th>
2026-08-29 11:47:39 +0700

    Update OJ3290.py

--- OJ3290.py
+++ OJ3290.py
@@ -4,10 +4,9 @@ def main():
     k = int(input())
     n = int(input())
     midle = int(n/2)+1
-    print(midle)
-    for i in range(n):
-        temp
-        for j in range(n-i):
-            print("*",end="")
-        print("-"*k)
+    for i in range(1,n+1):
+        for j in range(abs(midle-i)):
+            if i<  midle or i> midle:
+                print(" ",end="")
+        print("*"*k)
 main()
