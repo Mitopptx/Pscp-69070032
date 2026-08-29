9e5023d8ed67be8d10a33874cce01c4dc48944c3
Mitopptx <69070032@kmitl.ac.th>
2026-08-29 13:19:11 +0700

    Update OJ3290.py

--- OJ3290.py
+++ OJ3290.py
@@ -3,10 +3,10 @@ def main():
     """Arrow"""
     k = int(input())
     n = int(input())
-    midle = int(n/2)+1
     for i in range(1,n+1):
-        for j in range(abs(midle-i)):
-            if i<  midle or i> midle:
-                print(" ",end="")
-        print("*"*k)
+            print(" "*(n-i),end="")
+            print("*"*k)
+    for i in range(1,n):
+            print(" "*(i),end="")
+            print("*"*k)
 main()
