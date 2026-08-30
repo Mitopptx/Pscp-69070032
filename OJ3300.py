69c11f69d5d7807cd40cf8e1a484a43d981c1796
Mitopptx <69070032@kmitl.ac.th>
2026-08-30 13:33:47 +0700

    Create OJ3300.py

---
+++ OJ3300.py
@@ -0,0 +1,18 @@
+"""I'm tried so hard I got sofa"""
+def main():
+    """In the end"""
+    n = int(input())
+    heavy =0
+    chill = 0
+    for i in range(n):
+        hour = int(input())
+        if hour>18:
+            heavy +=1
+        else:
+            chill +=1
+    if heavy-chill>=1:
+        total = heavy + chill + (heavy-chill-1)
+    else:
+        total = heavy + chill
+    print(total)
+main()
