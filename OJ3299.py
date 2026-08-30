b706d9077e8edd173ae0427fd76123ba2f8f940b
Mitopptx <69070032@kmitl.ac.th>
2026-08-30 10:49:09 +0700

    Create OJ3299.py

---
+++ OJ3299.py
@@ -0,0 +1,16 @@
+"""stardewvalley"""
+def main():
+    """murakami"""
+    L,N = map(int,input().split())
+    count = 1
+    place = (startArea(L))
+    while N > place:
+        place += place+(L**2)
+        count+=1
+    print(count)
+def startArea(L):
+    if not L:
+        return 0
+    else:
+        return startArea(L-1)+L-1
+main()
