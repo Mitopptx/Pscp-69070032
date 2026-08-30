a2ea5e78183f9d0dc567e46f75d3386d83feb749
Mitopptx <69070032@kmitl.ac.th>
2026-08-30 14:37:36 +0700

    Update OJ3277MIDTERM.py

--- OJ3277MIDTERM.py
+++ OJ3277MIDTERM.py
@@ -1,39 +1,23 @@
 """anotherthai"""
 def main():
     """thainot plus"""
-    import math
     money = int(input())
-    day = int(input())
-    half =1000
+    days = int(input())
+    half = 1000
     buy = 0
-    exceed =0
-    for day in range(day):
+    for _ in range(days):
+        exceed = 0
         item = int(input())
-        for item in range(item):
+        for _ in range(item):
             price = int(input())
-            if (math.ceil(price*60/100) >=200 and half>0) and not exceed:
-                temp = 200
+            people = price * 40 //100
+            temp = min((price - people), 200 - exceed)
+            temp = min(temp, half)
+            pay = price - temp
+            if money >= pay:
+                money -= pay
                 half -= temp
-                price-= temp
-                exceed = temp
-            elif half>0 and exceed<200:
-                if exceed + math.ceil(price *60/100)>=200:
-                    temp = 200-exceed
-                    half -= temp
-                    price -= temp
-                    exceed =200
-                else:
-                    temp = math.ceil(price *60/100)
-                    half -= temp
-                    price -= temp
-                    exceed += temp
-            if money - price >=0:
-                money -= price
+                exceed += temp
                 buy +=1
-            else:
-                half +=temp
-                exceed -=temp
-        if half>0:
-            exceed = 0
-    print(buy,money,1000-half,sep="\n")
+    print(buy, money, 1000 - half, sep="\n")
 main()
