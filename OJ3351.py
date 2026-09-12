f02350e6fe749874cfd424cd2da5d01be74883a4
Mitopptx <69070032@kmitl.ac.th>
2026-09-12 11:27:09 +0700

    Create OJ3351.py

---
+++ OJ3351.py
@@ -0,0 +1,10 @@
+"""rabbit"""
+def main():
+    """rab"""
+    word = input()
+    word = word.lowwer()
+    for i in rage(word):
+        if word[i] == "a" and word[i-1] == "r":
+            count =1
+            
+main()
