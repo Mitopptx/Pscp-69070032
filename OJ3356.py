32e11a830c7ba65302dd4d3384c384ba97775c40
Mitopptx <69070032@kmitl.ac.th>
2026-09-12 14:41:31 +0700

    Create OJ3356.py

---
+++ OJ3356.py
@@ -0,0 +1,21 @@
+"""Bus"""
+def main():
+    """ahhhh"""
+    row = int(input())
+    col = int(input())
+    sit = int(input())
+    if not sit // 10:
+        sit = "0"+str(sit)
+    for i in range(row,0,-1):
+        if not i %2:
+            print("")
+        for j in range (col):
+            place = (row*j)+i
+            if not place //10:
+                place = "0"+str(place)
+            if place == sit:
+                print("XX",end=" ")
+            else:
+                print(place,end=" ")
+        print("")
+main()
