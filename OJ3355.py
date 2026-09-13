55b5a251bf42f744974be14809c5444c1f999bbe
Mitopptx <69070032@kmitl.ac.th>
2026-09-13 14:38:44 +0700

    Create OJ3355.py

---
+++ OJ3355.py
@@ -0,0 +1,22 @@
+"""cave"""
+def main():
+    """o-o"""
+    num = -2
+    number = []
+    short=""
+    while num != -1:
+        num = int(input())
+        number.append(num)
+    temp=0
+    for i in range(len(number)-1):
+        if i==len(number)-2:
+            short+=str(number[i])
+        elif number[i]+1==number[i+1]:
+            if not temp:
+                short+= str(number[i])+"-"
+                temp=1
+        else:
+            short += str(number[i])+","
+            temp=0
+    print(short)
+main()
