a2436fceebb6d5f70a38c2639a664161987ab1aa
Mitopptx <69070032@kmitl.ac.th>
2026-09-13 16:51:52 +0700

    Update OJ3351.py

--- OJ3351.py
+++ OJ3351.py
@@ -1,10 +1,36 @@
-"""rabbit"""
+"""raBBit"""
 def main():
-    """rab"""
+    """o-o"""
     word = input()
-    word = word.lowwer()
-    for i in rage(word):
-        if word[i] == "a" and word[i-1] == "r":
-            count =1
-            
+    word = word.upper()
+    macount=0
+    count=0
+    temp=0
+    index = -1
+    for i in range(len(word)):
+        if word[i] == "A":
+            if word[i-1]=="R":
+                count = 1
+                temp = 1
+            elif word[i-1]=="A":
+                count+=1
+            else:
+                temp =2
+                index = i 
+                break
+        elif word[i] =="B": 
+             if word[i+1] in ("I","T"):
+                 temp = 1
+             else:
+                 temp=2
+                 index = i
+                 break
+        if count>macount:
+              macount = count
+    if not temp:
+         print("unknown",len(word))
+    elif temp ==1:
+         print("yes",macount)
+    elif temp ==2:
+         print("no",index)
 main()
