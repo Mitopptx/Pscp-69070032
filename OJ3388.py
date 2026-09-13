9ee56c915fcd3900498e2f4d42bbc4b093bde96c
Mitopptx <69070032@kmitl.ac.th>
2026-09-13 12:41:22 +0700

    Create OJ3388.py

---
+++ OJ3388.py
@@ -0,0 +1,11 @@
+"""112"""
+def main():
+    """o-o"""
+    num = input()
+    number=""
+    for i in num:
+        number+=i
+        if "113" in number:
+            number = number.replace("113","")
+    print(number)
+main()
