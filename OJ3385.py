7fafed8146fe88e93c0c20d557486177598762a4
Mitopptx <69070032@kmitl.ac.th>
2026-09-13 12:19:29 +0700

    Create OJ3385.py

---
+++ OJ3385.py
@@ -0,0 +1,17 @@
+"""buss stop"""
+def main():
+    """o-o"""
+    pas = int(input())
+    stop = int(input())
+    arr =[]
+    bus = []
+    count =0
+    for i in range(1,stop+1):
+        arr = list(map(int,input().split()))
+        for j in range(1,len(arr)):
+            if arr[j]>i and len(bus)<5:
+                bus.append(arr[j])
+        count += bus.count(i+1)
+        bus = [j for j in bus if j!=i+1] 
+    print(count)
+main()
