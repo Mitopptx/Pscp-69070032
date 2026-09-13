dc91b204876c6269ad850b5d0a342161d8740928
Mitopptx <69070032@kmitl.ac.th>
2026-09-13 14:55:43 +0700

    Create OJ3357.py

---
+++ OJ3357.py
@@ -0,0 +1,20 @@
+"""Girafe furryyyy"""
+def main():
+    """o-o"""
+    n= int(input())
+    arr = []
+    count = 0
+    for _ in range(n):
+        h = int(input())
+        arr.append(h)
+    for i in range(n):
+        if not i:
+            if arr[i]>arr[i+1]:
+                count+=1
+        elif i == n-1:
+            if arr[i]> arr[i-1]:
+                count+=1
+        elif arr[i]>arr[i+1] and arr[i]>arr[i-1]:
+            count+=1
+    print(count)
+main()
