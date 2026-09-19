1428bf8a9013d71f5e93e9a832d30d55ca9536b0
Mitopptx <69070032@kmitl.ac.th>
2026-09-19 15:36:07 +0700

    Update OJ3362.py

--- OJ3362.py
+++ OJ3362.py
@@ -3,53 +3,51 @@ def main():
     """Siuuu"""
     one = input().upper()
     two = input().upper()
-    new = ""
-    j=0
+    new=""
     if len(one)>len(two):
-        for i in range(len(one)):
-            j=i
-            if i > len(two)-1:
-                j -= len(two)
-            new += two[j]
+        for i in range(0,len(one),len(two)):
+            for j in two:
+                new+=j
+                i+=1
+                if i >= len(one):
+                    break
+        new2 = one
     else:
-        for i in range(len(two)):
-            j=i
-            if i > len(one)-1:
-                j -= len(one)
-            new += one[j]
-            one = two
-    neww =""
-    count = 0
+         for i in range(0,len(two),len(one)):
+            for j in one:
+                new+=j
+                i+=1
+                if i >= len(two):
+                    break
+         new2 = two
+    neww = ""
+    count=0
     for i in range(len(new)):
-        if new[i] in ("L","O","V","E")or one[i] in ("L","O","V","E"):
+        if new[i] in ("L","O","V","E")or new2[i] in ("L","O","V","E"):
             neww += "w"
-            count += 1
+            count +=1
         else:
             neww += "$"
-    if count % 2:
-        count = 0
-        macount = 0
-        con = False
+    if not count %2:
         for i in range(len(neww)):
-            if neww[i] == "w":
-                if con == True:
-                    count += 1
-                else:
-                    count += 1
-                    con= True
-            else:
-                con ==False
-                count = 0
-            if macount < count:
-                macount = count
-        print(neww,macount,sep="")
+           if neww[i] == "w" and neww[i+1]=="w":
+               print(neww)
+               return
+        print(neww,"#",sep="")
     else:
-        for i in range(len(neww)):
-            if neww[i] == "w" and i!=len(neww):
-                if neww[i+1] == "w":
-                    print(neww)
-                    break
-                else:
-                    print(neww,"#",sep="")
-                    break
+       con = False
+       count=0
+       mac =0
+       for i in neww:
+           if i == "w":
+               if con:
+                   count +=1
+                   continue
+               con = True
+               count =1
+           else:
+                con = False
+           if mac < count:
+                mac = count
+       print(neww,mac,sep="")             
 main()
