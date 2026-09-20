67673f1fecdf7c6fd13c6854255a3d03a4f5f275
Mitopptx <69070032@kmitl.ac.th>
2026-09-20 11:47:01 +0700

    Create OJ3390.py

---
+++ OJ3390.py
@@ -0,0 +1,25 @@
+"""ARRAY 4DX"""
+def main():
+    """:3"""
+    one=list(map(int,input().split()))
+    two=list(map(int,input().split()))
+    three=list(map(int,input().split()))
+    four=list(map(int,input().split()))
+    five=list(map(int,input().split()))
+    first =-1
+    secound = -1
+    if sum(one)%2:
+        first = 0
+    elif sum(two)%2:
+        first = 1
+    elif sum(three)%2:
+        first = 2
+    elif sum(four)%2:
+        first = 3
+    elif sum(five)%2:
+        first = 4
+    for i in range(5):
+        if one[i]+two[i]+three[i]+four[i]+five[i] %2:
+            secound = i
+    print(first,secound)
+main()
